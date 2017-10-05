3c8910a06f956b276b4e2894a3bb7d48daf5db73
Steffen Mogensen <feber.sm@gmail.com>
2017-10-05 21:55:40 +0200

    make model for mysite

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class NewsPost(models.Model):
+    title = models.CharField(max_length=75)
+    text = models.TextField(max_length=10000)
+    image = models.ImageField(upload_to='news', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    subject = models.CharField(max_length=50)
+    text = models.CharField(max_length=4000)
+    post = models.ForeignKey(NewsPost, related_name='comments')
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='comments')
--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import NewsPost
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'site/index.html')
+    posts = NewsPost.objects.all()
+    return render(request, 'site/index.html', {'posts': posts})
